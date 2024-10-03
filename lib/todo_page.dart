dd16b9aff0cf87312f6a62d33bb775850af10833
Faik-irkham <faikirkham@gmail.com>
2024-10-04 05:49:22 +0700

    update

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -30,26 +30,53 @@ class _TodoPageState extends State<TodoPage> {
     prefs.setStringList('todos', _todoList);
   }
 
-  void _addTodoItem() {
-    if (_textController.text.isNotEmpty) {
-      setState(() {
-        _todoList.add(_textController.text);
-        _saveTodoList(); // Simpan setelah menambah item
-      });
-      _textController.clear();
-    }
+  void _showAddTodoDialog() {
+    _textController.clear();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Add To-Do Item'),
+          content: TextField(
+            controller: _textController,
+            decoration: const InputDecoration(
+              labelText: 'New To-Do Item',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                if (_textController.text.isNotEmpty) {
+                  setState(() {
+                    _todoList.add(_textController.text);
+                    _saveTodoList();
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Add'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void _removeTodoItem(int index) {
     setState(() {
       _todoList.removeAt(index);
-      _saveTodoList(); // Simpan setelah menghapus item
+      _saveTodoList();
     });
   }
 
   void _editTodoItem(int index) {
-    _textController.text =
-        _todoList[index]; // Set text controller dengan item yang akan diedit
+    _textController.text = _todoList[index];
     showDialog(
       context: context,
       builder: (context) {
@@ -67,8 +94,8 @@ class _TodoPageState extends State<TodoPage> {
               onPressed: () {
                 if (_textController.text.isNotEmpty) {
                   setState(() {
-                    _todoList[index] = _textController.text; // Update item
-                    _saveTodoList(); // Simpan setelah mengedit
+                    _todoList[index] = _textController.text;
+                    _saveTodoList();
                   });
                   Navigator.of(context).pop();
                 }
@@ -85,6 +112,13 @@ class _TodoPageState extends State<TodoPage> {
     );
   }
 
+  String capitalize(String text) {
+    if (text.isEmpty) {
+      return text;
+    }
+    return text[0].toUpperCase() + text.substring(1);
+  }
+
   @override
   void dispose() {
     _textController.dispose();
@@ -94,49 +128,102 @@ class _TodoPageState extends State<TodoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Simple To-Do List'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _textController,
-              decoration: const InputDecoration(
-                labelText: 'Enter To-Do Item',
-                border: OutlineInputBorder(),
+      body: Container(
+        color: Colors.green,
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'To-Do List',
+                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.white24,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        '${_todoList.length}',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: _addTodoItem,
-            child: const Text('Add Item'),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _todoList.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_todoList[index]),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.edit),
-                        onPressed: () => _editTodoItem(index),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () => _removeTodoItem(index),
-                      ),
-                    ],
+              const SizedBox(height: 25),
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
                   ),
-                );
-              },
-            ),
+                  child: ListView.builder(
+                    itemCount: _todoList.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 15),
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: Colors.grey.shade300,
+                            ),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              capitalize(_todoList[index]),
+                              style: Theme.of(context).textTheme.bodyLarge,
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  icon: const Icon(Icons.edit),
+                                  onPressed: () => _editTodoItem(index),
+                                ),
+                                IconButton(
+                                  icon: const Icon(Icons.delete),
+                                  onPressed: () => _removeTodoItem(index),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddTodoDialog,
+        backgroundColor: Colors.green,
+        child: const Icon(
+          Icons.add,
+          size: 40,
+          color: Colors.white,
+        ),
       ),
     );
   }
